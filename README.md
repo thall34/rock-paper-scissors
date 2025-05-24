585966fbb0c8895187371707fdbbef82c05ff647
Tyler <209157553+thall34@users.noreply.github.com>
2025-05-24 09:36:45 -0400

    Create game function that plays 5 rounds
    Create new function for playGame that contains function for playRound
    Set score to 0 for both players at the beginning of the function
    Loop the playRound function 5 times and log the scores each rounds
    Compare scores and declare a winner at the end of the function

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # rock-paper-scissors
 
 A Javascript program to play Rock Paper Scissors against a CPU opponent
-Accepts an input from the human user -> converts it to all lowercase
-Randomly generates a choice for the computer opponent
-Declares the winner after comparing the choices
+Accepts a string input from the human user -> converts it to all lowercase
+Randomly generates a choice for the computer opponent as a string
+Declares the winner after comparing the strings
 Increments the score up by 1 for the winner
-Game will be 5 rounds that keeps track of the overall score
+Game will be 5 rounds that keeps track of the overall score and declares a winner at the end
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,9 +2,7 @@
 // Generates a random choice from a computer opponent
 // Compares the two inputs and declares a winner
 // Increments the score for the winner
-
-let humanScore = 0
-let computerScore = 0
+// Declares a winner at the end of the game
 
 function getComputerChoice () {
     const randomInt = Math.random()
@@ -20,32 +18,49 @@ function getHumanChoice () {
     return string.toLowerCase()
 }
 
-function playRound (humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors!");
-        humanScore++
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beats Paper!");
-        humanScore++
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock!");
-        humanScore++
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats Rock!");
-        computerScore++
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beats Paper!");
-        computerScore++
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats Scissors!");
-        computerScore++
+function playGame () {
+
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound (humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors!");
+            humanScore++
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper!");
+            humanScore++
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore++
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock!");
+            computerScore++
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper!");
+            computerScore++
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++
+        } else
+            console.log("You are equally matched! No Points")
+    }
+
+    for (let round = 0; round < 5; round++) {
+        var humanSelection = getHumanChoice();
+        var computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection)
+            console.log("Human Score ", humanScore)
+            console.log("Computer Score ", computerScore);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You are the winner!")
+    } else if (humanScore < computerScore) {
+        console.log("Sorry, you lost.")
     } else
-        console.log("You are equally matched! No Points")
+        console.log("You tied! Better luck next time.")
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection)
-console.log("Human Score ", humanScore)
-console.log("Computer Score ", computerScore)
+playGame()
